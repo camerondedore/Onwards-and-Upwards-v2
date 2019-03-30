9a7a56b21e75e7048a573887403227ac88921d58
unknown <camerondedore@gmail.com>
2019-03-29 22:38:09 -0700

    Added human animations

---
+++ Assets/C#/test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    float projectileSpeed = 150;
+
+    [SerializeField]
+    GameObject target;
+    Rigidbody targetRigid;
+
+
+
+    void Start()
+    {
+        // get target
+        targetRigid = target.GetComponent<Rigidbody>();
+    }
+
+
+
+    void Update()
+    {
+        // get Pt
+        var targetDir = target.transform.position - transform.position;
+
+        // get a, b, c
+        var a = Mathf.Pow(targetDir.magnitude, 2) * Mathf.Pow(projectileSpeed, -2);
+        var b = Mathf.Pow(targetRigid.velocity.magnitude, 2) * Mathf.Pow(projectileSpeed, -2);
+        var c = 2 * targetDir.magnitude * targetRigid.velocity.magnitude * Vector3.Dot(targetDir.normalized, targetRigid.velocity.normalized) * Mathf.Pow(projectileSpeed, -2);
+
+        // calc aimDir
+        var aimDir = targetDir + targetRigid.velocity * (-c - Mathf.Sqrt(Mathf.Pow(c, 2) - 4 * (b - 1) * a)) / (2 * (b - 1));
+
+        transform.forward = aimDir;
+
+    }
+}
