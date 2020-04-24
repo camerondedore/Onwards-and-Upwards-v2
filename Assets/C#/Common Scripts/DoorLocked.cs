187afe89601dbe43a15810647227d341bdf3bd56
unknown <camerondedore@gmail.com>
2020-04-23 19:35:40 -0700

    adding locked door

---
+++ Assets/C#/Common Scripts/DoorLocked.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DoorLocked : MonoBehaviour
+{
+
+	[SerializeField]
+	AudioClip openSound,
+		closeSound,
+		lockedSound;
+	[SerializeField]
+	string pickupLog;
+	Animator[] anims;
+	AudioSourceController aud;
+	NavMeshObstacle obstacle;
+	List<Collider> colliders = new List<Collider>();
+	bool open = false,
+		locked = true;
+
+
+
+	void Start()
+	{
+		anims = GetComponentsInChildren<Animator>();
+		aud = GetComponent<AudioSourceController>();
+		obstacle = GetComponentInChildren<NavMeshObstacle>();
+	}
+
+
+
+	void OnTriggerEnter(Collider co)
+	{
+		if(locked)
+		{
+			// enter log
+			UiLog.Log(pickupLog);
+			aud.PlayOneShot(lockedSound);
+			return;
+		}
+
+		colliders.Add(co);
+
+		if (!open)
+		{
+			open = true;
+			aud.PlayOneShot(openSound);
+
+			foreach (var anim in anims)
+			{
+				anim.SetTrigger("open");
+			}
+		}
+	}
+
+
+
+	void OnTriggerExit(Collider co)
+	{
+		if (locked)
+		{
+			return;
+		}
+
+		colliders.Remove(co);
+
+		if (colliders.Count == 0 && open)
+		{
+			open = false;
+			aud.PlayOneShot(closeSound);
+
+			foreach (var anim in anims)
+			{
+				anim.SetTrigger("close");
+			}
+		}
+	}
+
+
+
+	public void Unlock()
+	{
+		locked = false;
+		obstacle.carving = false;
+	}
+}
---
+++ Assets/C#/Common Scripts/PickupDoorKey.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupDoorKey : MonoBehaviour, IPickup
+{
+
+	[SerializeField]
+	DoorLocked door;
+	[SerializeField]
+	AudioClip pickupSound;
+	[SerializeField]
+	string pickupLog;
+
+
+
+	void FixedUpdate()
+	{
+		transform.Rotate(Vector3.up, 90 * Time.fixedDeltaTime, Space.World);
+	}
+
+
+
+	public void Pickup(Transform player)
+	{		
+		door.Unlock();
+		// enter log
+		UiLog.Log(pickupLog);
+		AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+		Destroy(gameObject);
+	}
+}
