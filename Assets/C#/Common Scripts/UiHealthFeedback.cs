9723a8efcecefc5d51736313470f9b72fc9d8cf2
unknown <camerondedore@gmail.com>
2020-04-23 17:53:57 -0700

    switching to vertex lighting and adding health ui feedback

---
+++ Assets/C#/Common Scripts/UiHealthFeedback.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiHealthFeedback : MonoBehaviour
+{
+
+	Animator anim;
+	HealthPlayer health;
+	float lastHealth = -1;
+
+
+
+	void Start()
+	{
+		health = transform.root.GetComponentInChildren<HealthPlayer>();
+		anim = GetComponent<Animator>();
+		lastHealth = health.hitPoints;
+	}
+
+
+
+	void Update()
+	{
+		if(Time.timeScale == 0)
+		{
+			return;
+		}
+
+		if(lastHealth > health.hitPoints)
+		{
+			lastHealth = health.hitPoints;
+			anim.SetTrigger("hurt");	
+		}
+		
+		if (lastHealth < health.hitPoints)
+		{
+			lastHealth = health.hitPoints;
+			anim.SetTrigger("heal");
+		}
+	}
+}
