f131f9b132fea6f520f5701898163d0aa3ccbef9
unknown <camerondedore@gmail.com>
2020-05-24 17:52:18 -0700

    adding secret area

---
+++ Assets/C#/Common Scripts/SecretArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretArea : MonoBehaviour
+{
+    
+	[SerializeField] AudioClip secretSound;
+	string message = "Secret Area found";
+
+
+
+	void OnTriggerEnter()
+	{
+		// enter log
+		UiLog.Log(message);
+		AudioSource.PlayClipAtPoint(secretSound, transform.position);
+		Destroy(gameObject);
+	}
+}
